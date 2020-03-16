82fc205e5d0c09a6121b491489f6611764112aba
André Euzébio <andre.r.euzebio@gmail.com>
2020-03-16 06:39:23 -0300

    Working on reformulate page with new routes to stop using modal frame

--- Module03/Challenges03/public/scripts.js
+++ Module03/Challenges03/public/scripts.js
@@ -1,25 +1,10 @@
-const modalOverlay = document.querySelector(".modal-overlay");
 const courses = document.querySelectorAll(".Course");
-const modal = document.querySelector(".modal");
+
 
 for (let course of courses) {
     course.addEventListener("click",function() {
-        modalOverlay.classList.add("active");
         const courseClick = course.querySelector("span").getAttribute("id");
-        modalOverlay.querySelector("iframe").src = `https://rocketseat.com.br/${courseClick}`;
+        window.location.href = `courses/${courseClick}`;
     })
 }
 
-document.querySelector(".maximize-modal").addEventListener("click",function() {
-    if (modal.classList.contains("maximize")) {
-        return modal.classList.remove("maximize");
-    } 
-    
-    return modal.classList.add("maximize");
-    
-})
-
-document.querySelector(".close-modal").addEventListener("click",function() {
-    modalOverlay.classList.remove("active");
-    modalOverlay.querySelector("iframe").src = "";
-})
--- Module03/Challenges03/server.js
+++ Module03/Challenges03/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 
 const server = express();
+const courses = require('./data');
 
 server.use(express.static('public'));
 
@@ -34,25 +35,21 @@ server.get("/", function(req, res) {
 });
 
 server.get("/content", function(req, res) {
-    courses = [
-        { logo_url: "https://skylab.rocketseat.com.br/static/64c237ccff807c054339a62d53b4b402.svg",
-          title: '<span id="starter">Cursos 100% online e gratuitos</span> para você entrar com o pé direito nas tecnologias mais desejadas do mercado.',
-          modules: "7 módulos",
-          price: "Gratuito"  
-        },
-        { logo_url: "https://skylab.rocketseat.com.br/static/0828532024cb46921a6b5e941f8d788d.svg",
-          title: '<span id="launchbase">Domine programação do zero</span> e tenha acesso às melhores oportunidades do mercado no menor tempo possível.',
-          modules: "17 módulos",
-          price: "Pago"  
-        },
-        { logo_url: "https://skylab.rocketseat.com.br/static/83a178a0653dab1d55e2ed7946465975.svg",
-          title: 'Treinamento imersivo nas tecnologias mais modernas de desenvolvimento web e mobile para <span id="gostack">quem não tem tempo a perder</span>.',
-          modules: "25 módulos",
-          price: "Pago"  
-        }
-    ]
     return res.render("content", { courses });
 });
+
+server.get("/courses/:id", function(req, res) {
+    const id = req.params.id;
+
+    const course = courses.find(function(course) {
+        return course.id == id;
+    })
+    if (!course) {
+        return res.status(404).render("not-found");
+    }
+
+    return res.render("courses",{ course });
+  });
 
 server.listen(5000, function() {
     console.log("Server is running!");
